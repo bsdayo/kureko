84bb90be11ee133fc4eb98620303f2734b492d6e
bsdayo <bsdayo@outlook.com>
2025-07-20 23:39:22 +0800

    feat: add project link to footer

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="bg-secondary p-4 flex justify-between text-sm text-muted-foreground">
+  <footer class="bg-secondary p-4 flex justify-between text-sm text-muted-foreground">
     <div class="flex">
       <span>© {{ kurekoConfig?.footer.years }} {{ kurekoConfig?.author.name }}</span>
       <Separator v-if="kurekoConfig?.footer.icp?.enabled" class="mx-2" orientation="vertical" />
-      <a
-        v-if="kurekoConfig?.footer.icp?.enabled"
-        :href="kurekoConfig.footer.icp.link"
-        class="hover:underline underline-offset-4"
-      >
+      <a v-if="kurekoConfig?.footer.icp?.enabled" :href="kurekoConfig.footer.icp.link">
         {{ kurekoConfig.footer.icp.text }}
       </a>
     </div>
-    <div>Powered by Kureko</div>
-  </div>
+    <div>
+      Powered by
+      <a href="https://github.com/bsdayo/kureko">Kureko</a>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts" setup>
 const kurekoConfig = useKurekoConfig()
 </script>
+
+<style scoped>
+footer a:hover {
+  text-decoration-line: underline;
+  text-underline-offset: 4px;
+}
+</style>
