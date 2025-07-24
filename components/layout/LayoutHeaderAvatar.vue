fdffb6b4f9be2879c4ae6d5b918a98d60b21ee42
bsdayo <bsdayo@outlook.com>
2025-07-24 20:31:14 +0800

    feat: click avatar to navigate to /

--- components/layout/LayoutHeaderAvatar.vue
+++ components/layout/LayoutHeaderAvatar.vue
@@ -1,24 +1,13 @@
 <template>
-  <HoverCard :open-delay="0">
-    <HoverCardTrigger as-child>
-      <Avatar ref="avatarRef" class="size-10">
-        <AvatarImage :src="kurekoConfig?.author.avatarUrl ?? ''" :alt="kurekoConfig?.author.name" />
-        <AvatarFallback>{{ kurekoConfig?.author.name.slice(0, 2).toUpperCase() }}</AvatarFallback>
-      </Avatar>
-    </HoverCardTrigger>
-    <HoverCardContent align="start" :align-offset="-16">
-      <span class="font-mono font-bold">{{ kurekoConfig?.author.name }}</span>
-    </HoverCardContent>
-  </HoverCard>
+  <NuxtLink href="/">
+    <Avatar ref="avatarRef" class="size-10">
+      <AvatarImage :src="kurekoConfig?.author.avatarUrl ?? ''" :alt="kurekoConfig?.author.name" />
+      <AvatarFallback>{{ kurekoConfig?.author.name.slice(0, 2).toUpperCase() }}</AvatarFallback>
+    </Avatar>
+  </NuxtLink>
 </template>
 
 <script lang="ts" setup>
-import { onLongPress } from '@vueuse/core'
-
 const avatarRef = useTemplateRef<HTMLElement>('avatarRef')
 const kurekoConfig = useKurekoConfig()
-
-onLongPress(avatarRef, loginWithGitHub, {
-  delay: 5000,
-})
 </script>
